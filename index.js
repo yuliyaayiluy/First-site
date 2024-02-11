5527e4e32ec65339f40b035d072628e0130d2143
Юлия Мурашко <159699004+yuliyaayiluy@users.noreply.github.com>
2024-02-11 15:46:22 +0300

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log(1);
+
+const button = document.querySelector('button');
+const outpur = document.querySelector('.output');
+
+button.addEventListener('click',function() {
+    outpur.innerHTML = 'Миу';
+});
